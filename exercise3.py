af84685265f5f4992b39eb4fb77fe1fb4edd3147
Soxavin <138036135+Soxavin@users.noreply.github.com>
2023-08-02 01:36:46 +0700

    Create exercise3.py

---
+++ exercise3.py
@@ -0,0 +1,10 @@
+def remove_all_after(lst, border):
+    try:
+        index = lst.index(border)
+        return lst[:index+1]
+    except ValueError:
+        return lst
+
+
+print(remove_all_after([1, 2, 3, 4, 5], 3))
+print(remove_all_after([1, 1, 2, 2, 3, 3], 2))
