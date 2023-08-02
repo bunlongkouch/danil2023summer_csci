397f0b7951dfc15cbb815ad2460c70feba47811e
Soxavin <138036135+Soxavin@users.noreply.github.com>
2023-08-02 15:08:51 +0700

    Update exercise3.py

--- exercise3.py
+++ exercise3.py
@@ -1,10 +1,10 @@
-def remove_all_after(lst, border):
-    try:
-        index = lst.index(border)
-        return lst[:index+1]
-    except ValueError:
-        return lst
 
+def remove_all_after(numbers, n):
+    if n in numbers:
+        index = numbers.index(n)
+        return numbers[:index + 1]
+    else:
+        return numbers
 
-print(remove_all_after([1, 2, 3, 4, 5], 3))
-print(remove_all_after([1, 1, 2, 2, 3, 3], 2))
+print(remove_all_after([1, 2, 3, 4, 5], 3)) 
+print(remove_all_after([1, 1, 2, 2, 3, 3], 2)) 
